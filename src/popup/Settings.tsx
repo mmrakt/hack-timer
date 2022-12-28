3630eeb97b759c1d58c4975178642d3f6ba1bdfe
mmrakt <mimuraakkun3@gmail.com>
2022-12-28 23:09:59 +0900

    add settings content

--- src/popup/Settings.tsx
+++ src/popup/Settings.tsx
@@ -12,6 +12,8 @@ import {
 } from '../consts/index'
 import SettingButton from '../components/SettingButton'
 import { useTranslation } from 'react-i18next'
+import InputNumber from '../components/InputNumber'
+import InputToggle from '../components/InputToggle'
 
 const createBlobData = (dailyFocusedCounts: DailyFocusedCount[]): string => {
   const header = HISTORY_CSV_HEADER_ARRAY.join(',') + '\n'
@@ -115,20 +117,54 @@ const Settings: React.FC<{ handleDisplayTimer: () => void }> = ({
   }
 
   return (
-    <div className="w-48">
+    <div className="w-96">
       <div className="flex display-start mt-3 mx-3">
         <ArrowLeft handleClick={handleDisplayTimer} />
       </div>
-      <div className="text-lg">
-        {t('settings.import.confirm')}
-        <SettingButton
+      <div className="text-lg p-3">
+        <div className="mb-3">
+          <p className="text-2xl text-center">Timer</p>
+          <div className="mt-3 text-zinc-300">
+            <div className="flex items-center px-1">
+              <span className="class">Focus length</span>
+              <span className="ml-auto">
+                <InputNumber id="focusLength" />
+              </span>
+            </div>
+            <div className="flex items-center mt-2 px-1">
+              <span className="class">Short break length</span>
+              <span className="ml-auto">
+                <InputNumber id="shortBreakLength" />
+              </span>
+            </div>
+            <div className="flex items-center mt-2 px-1">
+              <span className="class">Long break length</span>
+              <span className="ml-auto">
+                <InputNumber id="longBreakLength" />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="boder-b-2">
+          <p className="text-2xl text-center">Notification</p>
+          <div className="mt-3 text-zinc-300">
+            <div className="flex items-center px-1">
+              <span className="class">Show new tab notification</span>
+              <span className="ml-auto">
+                <InputToggle id="newTabNotification" />
+              </span>
+            </div>
+          </div>
+        </div>
+        {/* <SettingButton
           handleClick={handleExport}
           text={EXPORT_CSV_BUTTON_TEXT}
         />
+        <div className="mt-5" />
         <SettingButton
           handleClick={handleOnChange}
           text={IMPORT_CSV_BUTTON_TEXT}
-        />
+        /> */}
       </div>
     </div>
   )
