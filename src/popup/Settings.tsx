d548ecd44c48bd4813b6416f7340f0f5c318d05c
mmrakt <mimuraakkun3@gmail.com>
2023-01-15 13:59:46 +0900

    refactor

--- src/popup/Settings.tsx
+++ src/popup/Settings.tsx
@@ -11,6 +11,7 @@ import {
   POMODORO_COUNT_UNTIL_LONG_BREAK
 } from '../consts/index'
 import Header from '../components/Header'
+import SettingRow from '../components/settings/SettingRow'
 
 const Settings: React.FC = () => {
   const { t } = useTranslation()
@@ -67,115 +68,77 @@ const Settings: React.FC = () => {
   return (
     <>
       <Header pageType="settings" />
-      <div className="text-lg p-3">
+      <div className="text-lg p-2">
         <div className="mb-3">
           <p className="text-lg text-center">{t('settings.timer.title')}</p>
           <div className="mt-3 text-zinc-300 text-sm">
-            <div className="flex items-center px-1">
-              <span className="class">
-                {t('settings.timer.length.pomodoro')}
-              </span>
-              <span className="ml-auto">
-                <TimerLengthSelect
-                  id="pomodoroSeconds"
-                  options={POMODORO_LENGTH_ARRAY}
-                  currentValue={pomodoroSeconds}
-                />
-              </span>
-            </div>
-            <div className="flex items-center mt-2 px-1">
-              <span className="class">{t('settings.timer.length.break')}</span>
-              <span className="ml-auto">
-                <TimerLengthSelect
-                  id="breakSeconds"
-                  options={BREAK_LENGTH_ARRAY}
-                  currentValue={breakSeconds}
-                />
-              </span>
-            </div>
-            <div className="flex items-center mt-2 px-1">
-              <span className="class">
-                {t('settings.timer.length.longBreak')}
-              </span>
-              <span className="ml-auto">
-                <TimerLengthSelect
-                  id="longBreakSeconds"
-                  options={LONG_BREAK_LENGTH_ARRAY}
-                  currentValue={longBreakSeconds}
-                />
-              </span>
-            </div>
-            <div className="flex items-center mt-2 px-1">
-              <span className="class">
-                {t('settings.timer.count.untilLongBreak')}
-              </span>
-              <span className="ml-auto">
-                <TimerLengthSelect
-                  id="pomodorosUntilLongBreak"
-                  options={POMODORO_COUNT_UNTIL_LONG_BREAK}
-                  currentValue={pomodorosUntilLongBreak}
-                />
-              </span>
-            </div>
+            <SettingRow label={t('settings.timer.length.pomodoro')}>
+              <TimerLengthSelect
+                id="pomodoroSeconds"
+                options={POMODORO_LENGTH_ARRAY}
+                currentValue={pomodoroSeconds}
+              />
+            </SettingRow>
+            <SettingRow label={t('settings.timer.length.break')}>
+              <TimerLengthSelect
+                id="breakSeconds"
+                options={BREAK_LENGTH_ARRAY}
+                currentValue={breakSeconds}
+              />
+            </SettingRow>
+            <SettingRow label={t('settings.timer.length.longBreak')}>
+              <TimerLengthSelect
+                id="longBreakSeconds"
+                options={LONG_BREAK_LENGTH_ARRAY}
+                currentValue={longBreakSeconds}
+              />
+            </SettingRow>
+            <SettingRow label={t('settings.timer.count.untilLongBreak')}>
+              <TimerLengthSelect
+                id="pomodorosUntilLongBreak"
+                options={POMODORO_COUNT_UNTIL_LONG_BREAK}
+                currentValue={pomodorosUntilLongBreak}
+              />
+            </SettingRow>
           </div>
         </div>
-        <div className="boder-b-2">
+        <div className="boder-b-2 mb-3">
           <p className="text-lg text-center">
             {t('settings.notification.title')}
           </p>
           <div className="mt-3 text-zinc-300 text-sm">
-            <div className="flex items-center px-1">
-              <span className="class">
-                {t('settings.notification.showNewTab.pomodoro')}
-              </span>
-              <span className="ml-auto">
-                {showNewTabNotificationWhenPomodoro !== null && (
-                  <SettingToggle
-                    id="showNewTabNotificationWhenPomodoro"
-                    currentValue={showNewTabNotificationWhenPomodoro}
-                  />
-                )}
-              </span>
-            </div>
-            <div className="flex items-center px-1">
-              <span className="class">
-                {t('settings.notification.desktop.pomodoro')}
-              </span>
-              <span className="ml-auto">
-                {showDesktopNotificationWhenPomodoro !== null && (
-                  <SettingToggle
-                    id="showDesktopNotificationWhenPomodoro"
-                    currentValue={showDesktopNotificationWhenPomodoro}
-                  />
-                )}
-              </span>
-            </div>
-            <div className="flex items-center px-1">
-              <span className="class">
-                {t('settings.notification.showNewTab.break')}
-              </span>
-              <span className="ml-auto">
-                {showNewTabpNotificationWhenBreak !== null && (
-                  <SettingToggle
-                    id="showNewTabNotificationWhenBreak"
-                    currentValue={showNewTabpNotificationWhenBreak}
-                  />
-                )}
-              </span>
-            </div>
-            <div className="flex items-center px-1">
-              <span className="class">
-                {t('settings.notification.desktop.break')}
-              </span>
-              <span className="ml-auto">
-                {showDesktopNotificationWhenBreak !== null && (
-                  <SettingToggle
-                    id="showDesktopNotificationWhenBreak"
-                    currentValue={showDesktopNotificationWhenBreak}
-                  />
-                )}
-              </span>
-            </div>
+            <SettingRow label={t('settings.notification.showNewTab.pomodoro')}>
+              {showNewTabNotificationWhenPomodoro !== null && (
+                <SettingToggle
+                  id="showNewTabNotificationWhenPomodoro"
+                  currentValue={showNewTabNotificationWhenPomodoro}
+                />
+              )}
+            </SettingRow>
+            <SettingRow label={t('settings.notification.desktop.pomodoro')}>
+              {showDesktopNotificationWhenPomodoro !== null && (
+                <SettingToggle
+                  id="showDesktopNotificationWhenPomodoro"
+                  currentValue={showDesktopNotificationWhenPomodoro}
+                />
+              )}
+            </SettingRow>
+            <SettingRow label={t('settings.notification.showNewTab.break')}>
+              {showNewTabpNotificationWhenBreak !== null && (
+                <SettingToggle
+                  id="showNewTabNotificationWhenBreak"
+                  currentValue={showNewTabpNotificationWhenBreak}
+                />
+              )}
+            </SettingRow>
+            <SettingRow label={t('settings.notification.desktop.break')}>
+              {showDesktopNotificationWhenBreak !== null && (
+                <SettingToggle
+                  id="showDesktopNotificationWhenBreak"
+                  currentValue={showDesktopNotificationWhenBreak}
+                />
+              )}
+            </SettingRow>
           </div>
         </div>
       </div>
